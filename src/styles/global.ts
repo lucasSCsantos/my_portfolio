bd85cbb6dba516aee2f47ea7ddf69ed510d833f6
lucasSCsantos <luc.cristovam10@gmail.com>
2021-11-25 17:05:17 -0300

    :tada: Begin project development
    Using template and installing dependencies, formating theme, installing fonts and removing not necessary issues

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,7 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
   }
 
   body {
@@ -13,4 +14,32 @@ export default createGlobalStyle`
     font: 400 16px Inter, sans-serif;
   }
 
+  img {
+    width: 100%;
+    max-width: 100%;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  .container {
+    width: 100%;
+    margin: 0 auto;
+    max-width: 85rem;
+    padding: 0 1rem;
+    @media(max-width:1440px) {
+      max-width: 70rem;
+    }
+    @media(max-width:1000px) {
+      max-width: 50rem;
+    }
+    @media(max-width:700px) {
+      padding: 0 2rem;
+    }
+  }
 `;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,15 @@
 const theme = {
   colors: {
-    background: '#121214',
-    text: '#e1e1e5',
-    primary: '#8257e6'
+    background: '#F2F2F2',
+    backgroundDark: '#0D0D0D',
+    text: '#000000',
+    textLight: '#F2F2F2',
+    primary: '#79BED9',
+    secondary: '#398CBF',
+    tertiary: '#1462A6',
+    quaternary: '#0F398C',
+    navHighlight: '#A3A3A3',
+    nav: '#CACACA'
   }
 };
 
