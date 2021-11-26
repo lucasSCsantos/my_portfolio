0823374f76a165796ce87559973423c290ad12dd
lucasSCsantos <luc.cristovam10@gmail.com>
2021-11-26 14:08:02 -0300

    :art: Creating typography components
    Creating heading and text default components and typing them

---
+++ src/components/DefaultComponents/index.ts
@@ -0,0 +1,42 @@
+import styled, { css } from 'styled-components';
+import theme from '../../styles/theme';
+
+export type HeadingProps = {
+  color?: keyof typeof theme.colors;
+  size?: keyof typeof theme.sizes;
+  fontWeight?: 100 | 400 | 700;
+  lineHeight?: string | number;
+  level?: 1 | 2 | 3 | 4 | 5 | 6;
+};
+
+export type TextProps = {
+  color?: keyof typeof theme.colors;
+  size?: keyof typeof theme.sizes;
+  fontWeight?: 100 | 400 | 700;
+  lineHeight?: string | number;
+};
+
+export const Heading = styled('h1').attrs<HeadingProps>(({ level = 1 }) => ({
+  as: `h${level}`
+}))<HeadingProps>`
+  ${({ color = 'text', size, fontWeight = 700, lineHeight = 1.5 }) => css`
+    font-size: ${theme.sizes[size]};
+    color: ${theme.colors[color]};
+    font-weight: ${fontWeight};
+    line-height: ${lineHeight};
+  `}
+`;
+
+export const Text = styled('p')<TextProps>`
+  ${({
+    color = 'text',
+    size = 'xsmall',
+    fontWeight = 700,
+    lineHeight = 1.5
+  }) => css`
+    font-size: ${theme.sizes[size]};
+    color: ${theme.colors[color]};
+    font-weight: ${fontWeight};
+    line-height: ${lineHeight};
+  `}
+`;
