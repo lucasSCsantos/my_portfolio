8a9a2766bb37257a141c7a15c9b7ebf45d1fa6d6
lucasSCsantos <luc.cristovam10@gmail.com>
2021-11-26 17:51:47 -0300

    :sparkles: Creating Header
    Creating Header component, and basic styling

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+import { Container } from './styles';
+
+function Header() {
+  return <Container />;
+}
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  width: 100%;
+  height: 7.5rem;
+  background-color: transparent;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
