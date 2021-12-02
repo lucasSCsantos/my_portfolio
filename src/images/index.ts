5566474305c87742f809cb2ba3fcd4c1000ac24b
lucasSCsantos <luc.cristovam10@gmail.com>
2021-12-02 11:23:25 -0300

    :sparkles: Creating Bubbles Branch
    Creating other branch to create bubbles component

--- src/images/index.ts
+++ src/images/index.ts
@@ -2,7 +2,7 @@ import js from './1024px-Unofficial_JavaScript_logo_2.svg.png';
 import ts from './Typescript_logo_2020.svg.png';
 import css from './css-icon-12.jpg';
 
-export const lenguages = [js, ts, css];
+export const lenguages = [js, ts];
 
 export const frameworks = [];
 
