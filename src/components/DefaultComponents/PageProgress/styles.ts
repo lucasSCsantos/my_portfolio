224262ed08b03170e7811da45e1704051330766c
lucasSCsantos <luc.cristovam10@gmail.com>
2021-12-03 20:49:04 -0300

    :lipstick: Update the style of page progress
    Changing colors and changing sizes from px to rem

--- src/components/DefaultComponents/PageProgress/styles.ts
+++ src/components/DefaultComponents/PageProgress/styles.ts
@@ -2,23 +2,24 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   position: fixed;
-  left: 20px;
+  left: 1.25rem;
   top: 40%;
   bottom: 50%;
   /* background-color: red; */
-  width: 100px;
-  height: 100px;
+  width: 6.25rem;
+  height: 6.25rem;
   z-index: 21;
   .actual {
-    width: 100px !important;
-    background-color: green;
+    width: 6.25rem !important;
+    background-color: ${({ theme }) => theme.colors.navHighlight};
   }
 `;
 
 export const BarContainer = styled.div`
-  height: 10px;
-  width: 80px;
-  margin-bottom: 20px;
-  background-color: red;
-  border-radius: 100px;
+  cursor: pointer;
+  height: 0.625rem;
+  width: 5rem;
+  margin-bottom: 1rem;
+  background-color: ${({ theme }) => theme.colors.nav};
+  border-radius: 6.25rem;
 `;
